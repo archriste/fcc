3c9be47f1ee8cba8ee56ed997957841aaac68d21
Alain Christe <alain-christetello@uiowa.edu>
2023-07-07 22:48:30 -0500

    add: timestamp project

---
+++ backend-development/project-timestamp/README.md
@@ -0,0 +1,5 @@
+# Timestamp Microservice
+
+This is the completed code for the freeCodeCamp Timestamp Microservice project based on the boilerplate.
+
+https://www.freecodecamp.org/learn/apis-and-microservices/apis-and-microservices-projects/timestamp-microservice
---
+++ backend-development/project-timestamp/index.js
@@ -0,0 +1,37 @@
+// index.js
+
+var express = require('express');
+var app = express();
+
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));
+
+app.use(express.static('public'));
+
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get("/api/:date", function (req, res) {
+  
+  const regex = new RegExp('^[0-9]+$')
+  let dateFromURL = req.params.date;
+  let parsedDate;
+
+  if(regex.test(dateFromURL)) {
+    parsedDate = parseInt(dateFromURL);
+  } else {
+    parsedDate = `${dateFromURL}T00:00:00`;
+  }
+
+  const parsedDateObject = new Date(parsedDate);
+
+  const returnString = parsedDateObject.toUTCString();
+  const timestamp = parsedDateObject.getTime();
+  
+  res.json({"unix":timestamp, "utc":returnString})
+});
+
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
